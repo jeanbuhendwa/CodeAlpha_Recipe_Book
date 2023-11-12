876211519be57459793750e1086e5dd0d2b9201c
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-12 14:10:40 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import "./App.css";
-// import RecipeList from "./components/recipeList";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RecipeList from "./components/recipeList";
 import RecipeForm from "./components/RecipeForm";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const storeRecipes = JSON.parse(localStorage.getItem("recipes") || []);
+    setRecipes(storeRecipes);
+  });
+  const saveRecipesToLocalStorage = (updatedRecipes) => {
+    setRecipes(updatedRecipes);
+    localStorage.setItem("recipes", JSON.stringify(updatedRecipes));
+  };
+
+  const handleAddRecipe = (newRecipe) => {
+    const updatedRecipes = [...recipes, { ...newRecipe, id: Date.now() }];
+    saveRecipesToLocalStorage(updatedRecipes);
+  };
+
+  const handleEditRecipe = (editedRecipe) => {
+    const updatedRecipes = recipes.map((recipe) =>
+      recipe.id === editedRecipe.id ? editedRecipe : recipe
+    );
+    saveRecipesToLocalStorage(updatedRecipes);
+  };
+
+  const handleDeleteRecipe = (recipeId) => {
+    const updatedRecipes = recipes.filter((recipe) => recipe.id !== recipeId);
+    saveRecipesToLocalStorage(updatedRecipes);
+  };
+
   return (
-    <div className="flex items-center justify-center text-4xl">
-      <RecipeForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact>
+          {/* Moved RecipeList outside the Routes */}
+          <RecipeList recipes={recipes} onDelete={handleDeleteRecipe} />
+        </Route>
+        <Route path="/add" exact>
+          <RecipeForm onSubmit={handleAddRecipe} />
+        </Route>
+        <Route path="/edit/:id" exact>
+          <RecipeForm
+            onSubmit={handleEditRecipe}
+            initialValues={recipes.find(
+              (recipe) =>
+                recipe.id.toString() ===
+                window.location.pathname.split("/").pop()
+            )}
+          />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 

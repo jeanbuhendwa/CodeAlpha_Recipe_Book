2b6f171f81ef9bf61f420f3b1f4370e6c0ebdf7f
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-12 14:10:54 +0300

    Update recipe

--- src/components/recipeList.jsx
+++ src/components/recipeList.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const RecipeList = () => {
-  return <div>RecipeList</div>;
+const RecipeList = ({ recipes, onDelete }) => {
+  return (
+    <section>
+      <h1>Recipe List</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <img src={recipe.image} alt={recipe.title} />
+            <h2>{recipe.title}</h2>
+            <p>{recipe.description}</p>
+            <Link to={`/edit/${recipe.id}`}>Edit Recipe</Link>
+            <button onClick={() => onDelete(recipe.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default RecipeList;

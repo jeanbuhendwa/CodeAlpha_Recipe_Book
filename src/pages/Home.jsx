67902529899a3eccb9d22ee2b11628e101794256
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-13 23:05:28 +0300

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,37 +4,31 @@ import { Link } from "react-router-dom";
 
 const Home = ({ recipes, deleteRecipe }) => {
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-2xl font-bold mb-4">Recipe Book</h1>
+    <div>
+      <h1>Recipe Book</h1>
       <ul>
         {recipes.map((recipe) => (
           <li key={recipe.id} className="mb-4">
-            <Link
-              to={`/edit/${recipe.id}`}
-              className="text-blue-500 hover:underline mr-2"
-            >
-              {recipe.title}
-            </Link>
-            <button
-              onClick={() => deleteRecipe(recipe.id)}
-              className="text-red-500 hover:underline"
-            >
-              Delete
-            </button>
             <div className="mt-2">
-              {recipe.imageUrl && (
-                <a
-                  href={recipe.imageUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={recipe.imageUrl}
-                    alt={recipe.title}
-                    className="max-w-full h-auto border rounded-md"
-                  />
-                </a>
-              )}
+              <img
+                src={recipe.imageUrl}
+                alt={recipe.title}
+                className="w-full h-auto border rounded-md"
+              />
+            </div>
+            <div>
+              <h2 className="text-lg font-bold">{recipe.title}</h2>
+              <p>{recipe.instructions}</p>
+              <p>Ingredients: {recipe.ingredients}</p>
+            </div>
+            <div>
+              <Link to={`/edit/${recipe.id}`}>Edit</Link>
+              <button
+                onClick={() => deleteRecipe(recipe.id)}
+                className="text-red-500 hover:underline"
+              >
+                Delete
+              </button>
             </div>
           </li>
         ))}

7cf7f38bc31b620c67376b2716ea6029954dbca9
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-15 23:38:39 +0300

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-// src/pages/Home.js
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = ({ recipes, deleteRecipe }) => {
   return (
-    <div>
-      <h1>Recipe Book</h1>
-      <ul>
+    <div className="pt-[100px] px-3 md:px-[120px] flex flex-col items-center justify-center">
+      <h1 className="text-[#461212] font-bold text-3xl my-5">Recipe Book</h1>
+      <ul className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
         {recipes.map((recipe) => (
-          <li key={recipe.id} className="mb-4">
+          <li
+            key={recipe.id}
+            className="mb-4 border-4 border-red-950 bg-primary bg-opacity-[0.1] backdrop-blur-sm rounded-[8px] py-2 px-4 grid grid-rows-3"
+          >
             <div className="mt-2">
               <img
                 src={recipe.imageUrl}

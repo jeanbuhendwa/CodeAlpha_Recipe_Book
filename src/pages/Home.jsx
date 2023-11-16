60d67d354fbc8c5768c03c85ab5b86a3e87e064d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-16 15:14:56 +0300

    Update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,25 +9,40 @@ const Home = ({ recipes, deleteRecipe }) => {
         {recipes.map((recipe) => (
           <li
             key={recipe.id}
-            className="mb-4 border-4 border-red-950 bg-primary bg-opacity-[0.1] backdrop-blur-sm rounded-[8px] py-2 px-4 grid grid-rows-3"
+            className="mb-4 shadow-inner bg-primary bg-opacity-[0.1] backdrop-blur-sm rounded-[8px] py-2 px-4 pb-5"
           >
             <div className="mt-2">
               <img
                 src={recipe.imageUrl}
                 alt={recipe.title}
-                className="w-full h-auto border rounded-md"
+                className="w-full h-[200px] border rounded-md mb-4"
               />
             </div>
-            <div>
-              <h2 className="text-lg font-bold">{recipe.title}</h2>
-              <p>{recipe.instructions}</p>
-              <p>Ingredients: {recipe.ingredients}</p>
+
+            <div className="h-[130px]">
+              <h2 className="text-lg font-bold text-[#461212] mb-2">
+                {recipe.title}
+              </h2>
+              <p className="text-primary font-semibold mb-2 text-sm">
+                {recipe.instructions}
+              </p>
+              <p className="text-primary font-semibold mb-4">
+                Ingredients:{" "}
+                <span className="italic font-normal mb-2">
+                  {recipe.ingredients}
+                </span>
+              </p>
             </div>
-            <div>
-              <Link to={`/edit/${recipe.id}`}>Edit</Link>
+            <div className="flex justify-center items-center gap-4">
+              <Link
+                to={`/edit/${recipe.id}`}
+                className="text-red-500 border-2 border-red-500 py-1 px-5 rounded-lg"
+              >
+                Edit
+              </Link>
               <button
                 onClick={() => deleteRecipe(recipe.id)}
-                className="text-red-500 hover:underline"
+                className="text-white bg-red-500 py-1 px-5 rounded-lg"
               >
                 Delete
               </button>
